c4503d3a8f94b34eaba773a10e70394966b186c1
geonsu-pak <82432733+geonsu-pak@users.noreply.github.com>
2021-05-03 23:56:54 +0900

    Update New_Xception.py

--- New_Xception.py
+++ New_Xception.py
@@ -1,36 +1,27 @@
-import numpy as np
+import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-import tensorflow_datasets as tfds
+from tensorflow.keras import layers
+
 
 tfds.disable_progress_bar()
 
-# To keep our dataset small, we will use 40% of the original training data (25,000 images) for training, 10% for validation, and 10% for testing.
-train_ds, validation_ds, test_ds = tfds.load(
-    "cats_vs_dogs",
-    # Reserve 10% for validation and 10% for test
-    split=["train[:40%]", "train[40%:50%]", "train[50%:60%]"],
-    as_supervised=True,  # Include labels
-)
+train_ds, validation_ds, test_ds = tfds.load('cats_vs_dogs',
+                                             split=["train[:40%]", "train[40%:50%]", "train[50%:60%]"],
+                                             as_supervised=True)
 
 print("Number of training samples: %d" % tf.data.experimental.cardinality(train_ds))
-print(
-    "Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds)
-)
+print("Number of validation samples: %d" % tf.data.experimental.cardinality(validation_ds))
 print("Number of test samples: %d" % tf.data.experimental.cardinality(test_ds))
 
 plt.figure(figsize=(10, 10))
 for i, (image, label) in enumerate(train_ds.take(9)):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(image)
-    plt.title(int(label))
-    plt.axis("off")
-    
-# Standardizing the data
-#  We need to do 2 things:
-#  Standardize to a fixed image size. We pick 150x150.
-#  Normalize pixel values between -1 and 1. We'll do this using a Normalization layer as part of the model itself.
+  ax = plt.subplot(3, 3, i + 1)
+  plt.imshow(image)
+  plt.title(int(label))
+  plt.axis("off")
+
 size = (150, 150)
 
 train_ds = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
@@ -43,4 +34,105 @@ train_ds = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
 validation_ds = validation_ds.cache().batch(batch_size).prefetch(buffer_size=10)
 test_ds = test_ds.cache().batch(batch_size).prefetch(buffer_size=10)
 
+data_augmentation = keras.Sequential(
+    [
+     layers.experimental.preprocessing.RandomFlip("horizontal"),
+     layers.experimental.preprocessing.RandomRotation(0.1),
+     ]
+     )
+
+import numpy as np
+
+for images, labels in train_ds.take(1):
+  plt.figure(figsize=(10, 10))
+  first_image = images[0]
+  for i in range(9):
+    ax = plt.subplot(3, 3, i + 1)
+    augmented_image = data_augmentation(
+        tf.expand_dims(first_image, 0), training=True
+        )
+    plt.imshow(augmented_image[0].numpy().astype("int32"))
+    plt.title(int(labels[0]))
+    plt.axis("off")
+
+base_model = keras.applications.Xception(
+    weights="imagenet",
+    input_shape=(150, 150, 3),
+    include_top=False,
+    )
+
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+x = data_augmentation(inputs)
+
+# Pre-trained Xception weights requires that input be normalized
+# from (0, 255) to a range (-1., +1.), the normalization layer
+# does the following, outputs = (inputs - mean) / sqrt(var)
+norm_layer = keras.layers.experimental.preprocessing.Normalization()
+mean = np.array([127.5] * 3)
+var = mean ** 2
+# Scale inputs to [-1, +1]
+x = norm_layer(x)
+norm_layer.set_weights([mean, var])
+
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)
+# Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary()
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+    )
+
+epochs = 20
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+# Unfreeze the base_model. Note that it keeps running in inference mode
+# since we passed `training=False` when calling it. This means that
+# the batchnorm layers will not update their batch statistics.
+# This prevents the batchnorm layers from undoing all the training
+# we've done so far.
+base_model.trainable = True
+model.summary()
+
+# Low learning rate
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-5),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+    )
+
+epochs = 10
+history = model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Test'], loc='upper left')
+plt.show()
 
